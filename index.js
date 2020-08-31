40639886d6a771209100c78a2343cec44de9d47b
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2020-08-30 22:57:11 -0400

    Handled Errors using winston library

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
-const express = require('express');
+const winston = require("winston");
+require("express-async-errors");
+require("winston-mongodb");
+const express = require("express");
 const config = require("config");
 const jwt = require("jsonwebtoken");
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 
 const app = express();
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const mongoose = require('mongoose')
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const mongoose = require("mongoose");
 
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const error = require("./middleware/error");
+
+// process.on('uncaughtException',(ex)=>{
+//   console.log('UNCAUGHT EXCEPTION:');
+//   winston.error(ex.message,ex);
+//process.exit(1)
+// })
+// process.on("unhandledRejection", (ex) => {
+//   console.log("UNHANDLED REJECTION:");
+//   winston.error(ex.message, ex);
+//   process.exit(1);
+// });
+winston.handleExceptions(new winston.transports.File({filename: "oncaughtexceptions.log"}))
+winston.add(new winston.transports.File({ filename: "logfile.log" })); //created our own transport
+winston.add(
+  new winston.transports.MongoDB({
+    db: "mongodb://localhost/vidly",
+    level: "error",
+  })
+);
+//throw new Error('Something failed at startup')
+// const p = Promise.reject(new Error("Something failed miserably!"));
+// p.then(()=> console.log(''))
 
 //const home = require('./routes/home');
-if(!config.get('privatekey')){
-  console.error('Fatal Error: privatekey is not defined')
-  process.exit(1)
+if (!config.get("privatekey")) {
+  console.error("Fatal Error: privatekey is not defined");
+  process.exit(1);
 }
 app.use(express.json());
-app.use('/vidly.com/api/genres', genres);
-app.use('/vidly.com/api/customers', customers);
-app.use('/vidly.com/api/movies', movies);
-app.use('/vidly.com/api/rentals', rentals);
-app.use('/vidly.com/api/users', users);
-app.use('/vidly.com/api/auth', auth);
+app.use("/vidly.com/api/genres", genres);
+app.use("/vidly.com/api/customers", customers);
+app.use("/vidly.com/api/movies", movies);
+app.use("/vidly.com/api/rentals", rentals);
+app.use("/vidly.com/api/users", users);
+app.use("/vidly.com/api/auth", auth);
+app.use(error);
+
+//app.use(error); //passing the refrence to function
 //app.use(logger);
 //app.use('/', home);
 //---------------------------------------------------------------------
@@ -38,8 +67,8 @@ mongoose
   .then(() => console.log("Connected to Mongodb.."))
   .catch((err) => console.log("Could not connect to Mongodb..", err));
 
-  //--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
- console.log(`Listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
--- routes/genres.js
+++ routes/genres.js
@@ -1,5 +1,6 @@
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
+//const async = require("../middleware/async");
 const express = require("express");
 
 const router = express.Router();
@@ -8,9 +9,10 @@ const { Genre, validate } = require("../models/genre");
 const mongoose = require("mongoose");
 
 //---------------------------------------------------------------------------
-
+ 
 //-------------------------------------------------------------------------
 router.get("/", async (req, res) => {
+  //throw new Error('Could not get genres');
   res.send(await Genre.find().sort("name"));
 });
 //-------------------------------------------------------------------------
@@ -45,10 +47,11 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 //--------------------------------------------------------------------------
-router.delete("/:id", [auth,admin],async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
-  if (!genre) return res.status(404).send("The genre with the given ID was not found.");
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
 
   res.send(genre);
 });
